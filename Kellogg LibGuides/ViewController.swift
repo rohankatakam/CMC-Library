18d15e06b07a9b4b7ff4bf631b655ed1c101eb8f
Rohan Katakam <rohankatakam@yahoo.com>
2017-06-28 09:54:23 -0500

    Initial Commit

---
+++ Kellogg LibGuides/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kellogg LibGuides
+//
+//  Created by Student on 6/28/17.
+//  Copyright © 2017 Rohan Katakam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
