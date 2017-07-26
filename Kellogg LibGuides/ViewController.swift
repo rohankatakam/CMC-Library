e843cf80f1c0de0409a768d78da30a3331d98596
Rohan <rohan@dhcp-10-105-131-61.wireless.northwestern.private>
2017-07-26 12:39:04 -0500

    Unnecessary Databases Hidden

--- Kellogg LibGuides/ViewController.swift
+++ Kellogg LibGuides/ViewController.swift
@@ -132,24 +132,282 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                         guideURLs.append(element["url"]! as! String)
                     }
                     
+                   
                     
-                    /*
+                    display(arr: guideNames)
+                    print(guideNames.count)
+                    print("---------------------")
+                    
+                    
+                    display(arr: guideURLs)
+                    print(guideURLs.count)
+                    print("---------------------")
+
+                    
+                    display(arr: guideLogos)
+                    print(guideLogos.count)
+                    print("---------------------")
+
+                    
+                    display(arr: guideDescriptions)
+                    print(guideDescriptions.count)
+                    
+                    
+                    print("---------------------")
+                    
+                    
+                    print("Deleted Set")
+                    
+                    //Remove Capital IQ
+                    let capitalIQ = getIndex(arr: guideNames, filter: "Capital IQ")
+                    
+                    guideNames.remove(at: capitalIQ)
+                    guideURLs.remove(at: capitalIQ)
+                    guideLogos.remove(at: capitalIQ)
+                    guideDescriptions.remove(at: capitalIQ)
+                    
+                    
+                    
+                    //Remove Thomson Investext
+                    
+                    let thomson = getIndex(arr: guideNames, filter: "Thomson Investext")
                      
-                    //Remove Factiva
+                    guideNames.remove(at: thomson)
+                    guideURLs.remove(at: thomson)
+                    guideLogos.remove(at: thomson)
+                    guideDescriptions.remove(at: thomson)
                     
-                    var index = getIndex(arr: guideNames, filter: "Factiva")
                     
-                    guideNames.remove(at: index)
-                    guideURLs.remove(at: index)
-                    guideLogos.remove(at: index)
+                    
+                    //Remove Emerging Markets Information Service (EMIS)
                      
-                     */
+                    let emis = getIndex(arr: guideNames, filter: "Emerging Markets Information Service (EMIS)")
+                     
+                    guideNames.remove(at: emis)
+                    guideURLs.remove(at: emis)
+                    guideLogos.remove(at: emis)
+                    guideDescriptions.remove(at: emis)
+                    
+                    
+                    
+                    //Remove Associations unlimited
+                    let associations = getIndex(arr: guideNames, filter: "Associations Unlimited")
+                    
+                    guideNames.remove(at: associations)
+                    guideURLs.remove(at: associations)
+                    guideLogos.remove(at: associations)
+                    guideDescriptions.remove(at: associations)
+                    
+                    //Remove Datastream
+                    let datastream = getIndex(arr: guideNames, filter: "Datastream")
+                    
+                    guideNames.remove(at: datastream)
+                    guideURLs.remove(at: datastream)
+                    guideLogos.remove(at: datastream)
+                    guideDescriptions.remove(at: datastream)
+                    
+                    //Remove FactSet
+                    let factset = getIndex(arr: guideNames, filter: "FactSet")
+                    
+                    guideNames.remove(at: factset)
+                    guideURLs.remove(at: factset)
+                    guideLogos.remove(at: factset)
+                    guideDescriptions.remove(at: factset)
+                    
+                    //Remove Financial Times
+                    let finanacialTimes = getIndex(arr: guideNames, filter: "Financial Times")
+                    
+                    guideNames.remove(at: finanacialTimes)
+                    guideURLs.remove(at: finanacialTimes)
+                    guideLogos.remove(at: finanacialTimes)
+                    guideDescriptions.remove(at: finanacialTimes)
+                    
+                    //Remove Gale CaseBase: Case Studies in Global Business
+                    let gale = getIndex(arr: guideNames, filter: "Gale CaseBase: Case Studies in Global Business")
+                    
+                    guideNames.remove(at: gale)
+                    guideURLs.remove(at: gale)
+                    guideLogos.remove(at: gale)
+                    guideDescriptions.remove(at: gale)
+                    
+                    //Remove RCA: Real Capital Analytics
+                    let rca = getIndex(arr: guideNames, filter: "RCA: Real Capital Analytics")
+                    
+                    guideNames.remove(at: rca)
+                    guideURLs.remove(at: rca)
+                    guideLogos.remove(at: rca)
+                    guideDescriptions.remove(at: rca)
+                    
+                    //Remove BoardEx
+                    let board = getIndex(arr: guideNames, filter: "BoardEx")
+                    
+                    guideNames.remove(at: board)
+                    guideURLs.remove(at: board)
+                    guideLogos.remove(at: board)
+                    guideDescriptions.remove(at: board)
+                    
+                    //Remove SNL
+                    let snl = getIndex(arr: guideNames, filter: "SNL")
+                    
+                    guideNames.remove(at: snl)
+                    guideURLs.remove(at: snl)
+                    guideLogos.remove(at: snl)
+                    guideDescriptions.remove(at: snl)
+                    
+                    //Remove International Directory of Company Histories
+                    let international = getIndex(arr: guideNames, filter: "International Directory of Company Histories")
+                    
+                    guideNames.remove(at: international)
+                    guideURLs.remove(at: international)
+                    guideLogos.remove(at: international)
+                    guideDescriptions.remove(at: international)
+                    
+                    //Remove TechNavio Global Market Assessment Reports (EMIS)
+                    let techNavioGlobal = getIndex(arr: guideNames, filter: "TechNavio Global Market Assessment Reports (EMIS)")
+                    
+                    guideNames.remove(at: techNavioGlobal)
+                    guideURLs.remove(at: techNavioGlobal)
+                    guideLogos.remove(at: techNavioGlobal)
+                    guideDescriptions.remove(at: techNavioGlobal)
+                    
+                    //Remove TechNavio Market Assessment Country Reports (EMIS)
+                    let techNavioMarket = getIndex(arr: guideNames, filter: "TechNavio Market Assessment Country Reports (EMIS)")
+                    
+                    guideNames.remove(at: techNavioMarket)
+                    guideURLs.remove(at: techNavioMarket)
+                    guideLogos.remove(at: techNavioMarket)
+                    guideDescriptions.remove(at: techNavioMarket)
+                    
+                    //Remove TechNavio Regional Market Assessment Reports (EMIS)
+                    let techNavioRegional = getIndex(arr: guideNames, filter: "TechNavio Regional Market Assessment Reports (EMIS)")
+                    
+                    guideNames.remove(at: techNavioRegional)
+                    guideURLs.remove(at: techNavioRegional)
+                    guideLogos.remove(at: techNavioRegional)
+                    guideDescriptions.remove(at: techNavioRegional)
+                     
+                    //Remove Smart Research - Local Market Research (EMIS)
+                    let smartResearch = getIndex(arr: guideNames, filter: "Smart Research - Local Market Research (EMIS)")
+                    
+                    guideNames.remove(at: smartResearch)
+                    guideURLs.remove(at: smartResearch)
+                    guideLogos.remove(at: smartResearch)
+                    guideDescriptions.remove(at: smartResearch)
+                    
+                    //Remove Smart Research - Company Profiles (EMIS)
+                    let smartResearchCompany = getIndex(arr: guideNames, filter: "Smart Research - Company Profiles (EMIS)")
+                    
+                    guideNames.remove(at: smartResearchCompany)
+                    guideURLs.remove(at: smartResearchCompany)
+                    guideLogos.remove(at: smartResearchCompany)
+                    guideDescriptions.remove(at: smartResearchCompany)
+                    
+                    //Remove Smart Research - Global Market Research Reports (EMIS)
+                    let smartResearchGlobal = getIndex(arr: guideNames, filter: "Smart Research - Global Market Research Reports (EMIS)")
+                    
+                    guideNames.remove(at: smartResearchGlobal)
+                    guideURLs.remove(at: smartResearchGlobal)
+                    guideLogos.remove(at: smartResearchGlobal)
+                    guideDescriptions.remove(at: smartResearchGlobal)
+                    
+                    //Remove Parallaxis Consulting Sector Reports (EMIS)
+                    let parallaxis = getIndex(arr: guideNames, filter: "Parallaxis Consulting Sector Reports (EMIS)")
+                    
+                    guideNames.remove(at: parallaxis)
+                    guideURLs.remove(at: parallaxis)
+                    guideLogos.remove(at: parallaxis)
+                    guideDescriptions.remove(at: parallaxis)
+                    
+                    //Remove ACM Sector Reports (EMIS)
+                    let acm = getIndex(arr: guideNames, filter: "ACM Sector Reports (EMIS)")
+                    
+                    guideNames.remove(at: acm)
+                    guideURLs.remove(at: acm)
+                    guideLogos.remove(at: acm)
+                    guideDescriptions.remove(at: acm)
+                    
+                    
+                    //Remove Fitch Ratings Special Reports (EMIS)
+                    let fitchRatings = getIndex(arr: guideNames, filter: "Fitch Ratings Special Reports (EMIS)")
+                    
+                    guideNames.remove(at: fitchRatings)
+                    guideURLs.remove(at: fitchRatings)
+                    guideLogos.remove(at: fitchRatings)
+                    guideDescriptions.remove(at: fitchRatings)
+                    
+                    //Remove Global Data- Industry Snapshots (EMIS)
+                    let globalData = getIndex(arr: guideNames, filter: "Global Data- Industry Snapshots (EMIS)")
+                    
+                    guideNames.remove(at: globalData)
+                    guideURLs.remove(at: globalData)
+                    guideLogos.remove(at: globalData)
+                    guideDescriptions.remove(at: globalData)
+                    
+                    //Remove Euromonitor Sector Capsules (EMIS)
+                    let euromonitorSector = getIndex(arr: guideNames, filter: "Euromonitor Sector Capsules (EMIS)")
+                    
+                    guideNames.remove(at: euromonitorSector)
+                    guideURLs.remove(at: euromonitorSector)
+                    guideLogos.remove(at: euromonitorSector)
+                    guideDescriptions.remove(at: euromonitorSector)
+                    
+                    //Remove Euromonitor- Industry Capsules (EMIS)
+                    let euromonitorIndustry = getIndex(arr: guideNames, filter: "Euromonitor- Industry Capsules (EMIS)")
+                    
+                    guideNames.remove(at: euromonitorIndustry)
+                    guideURLs.remove(at: euromonitorIndustry)
+                    guideLogos.remove(at: euromonitorIndustry)
+                    guideDescriptions.remove(at: euromonitorIndustry)
+                    
+                    //Remove BMI Research - Industry Reports (EMIS)
+                    let bmi = getIndex(arr: guideNames, filter: "BMI Research - Industry Reports (EMIS)")
+                    
+                    guideNames.remove(at: bmi)
+                    guideURLs.remove(at: bmi)
+                    guideLogos.remove(at: bmi)
+                    guideDescriptions.remove(at: bmi)
+                    
+                    //Remove GRDS Industry Reports (EMIS)
+                    let grds = getIndex(arr: guideNames, filter: "GRDS Industry Reports (EMIS)")
+                    
+                    guideNames.remove(at: grds)
+                    guideURLs.remove(at: grds)
+                    guideLogos.remove(at: grds)
+                    guideDescriptions.remove(at: grds)
+                    
+                    //Remove Prequin
+                    let prequin = getIndex(arr: guideNames, filter: "Preqin")
+                    
+                    guideNames.remove(at: prequin)
+                    guideURLs.remove(at: prequin)
+                    guideLogos.remove(at: prequin)
+                    guideDescriptions.remove(at: prequin)
+                    
+                    //Remove IPREO
+                    let ipreo = getIndex(arr: guideNames, filter: "IPREO Bigdough")
+                    
+                    guideNames.remove(at: ipreo)
+                    guideURLs.remove(at: ipreo)
+                    guideLogos.remove(at: ipreo)
+                    guideDescriptions.remove(at: ipreo)
+                    
                     
                     DispatchQueue.main.sync {
                         self.data = guideNames
+                        display(arr: guideNames)
+                        print(guideNames.count)
+                        print("---------------------")
                         self.urls = guideURLs
+                        display(arr: guideURLs)
+                        print(guideURLs.count)
+                        print("---------------------")
                         self.logos = guideLogos
+                        display(arr: guideLogos)
+                        print(guideLogos.count)
+                        print("---------------------")
                         self.descriptions = guideDescriptions
+                        display(arr: guideDescriptions)
+                        print(guideDescriptions.count)
                         self.tableView.reloadData()
                     }
                     
@@ -205,6 +463,12 @@ func getIndex(arr: [String], filter: String) -> Int {
     }
     
     return returnVal
+}
+
+func display(arr: [String]){
+    for element in arr {
+        print(element + "\n")
+    }
 }
 
 extension String {
